7eb5fe86e1ec8f4f35d761da0f04652f9357ce5b
Daniel lara <daniellaraedinburgh@hotmail.com>
2018-10-13 12:49:44 +0100

    css perspective

---
+++ CSS:SCSS-PG/cssPerspective/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.get('/', function(request, response){
+ response.sendFile(__dirname + '/client/index.html')
+})
+
+app.use(bodyParser.json());
+app.use(express.static('client'))
+
+app.listen(3000, function(){
+    console.log("estas conectado cabronazo")
+})
