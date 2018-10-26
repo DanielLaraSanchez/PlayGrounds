012c4a89d7e6238a3e1204ea6f7dfced876231b1
Daniel lara <daniellaraedinburgh@hotmail.com>
2018-10-26 18:49:28 +0100

    codingtraing coding challenge 1

---
+++ JAVASCRIPT-PG/CODINGTRAIN-PG/CodingChallenge67Pong/client/js/main.js
@@ -0,0 +1,22 @@
+
+let snow = []
+
+
+
+
+
+
+function setup() { 
+    createCanvas(windowWidth, windowHeight);
+  } 
+  
+  function draw() { 
+    background(0);
+    snow.push(new Snowflake());
+
+
+    for(flake of snow){
+        flake.render();
+
+    }
+  }
